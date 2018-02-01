e662b78ef70f328df33332604ea5ba27fd3caa0d
declanmurphy7 <35809332+declanmurphy7@users.noreply.github.com>
2018-02-01 16:20:36 +0000

    Version 2

---
+++ js/array-literal.js
@@ -0,0 +1,8 @@
+var colors = new Array('white',
+					   'black',
+					   'red',
+					   'beige');
+colors.push("purple");					
+
+var el = document.getElementById('colors');
+el.textContent = colors[4];
